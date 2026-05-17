b630009631fc613e1670b77020ba9ac650b08492
RoHsk14 <rodriguehessou14@gmail.com>
2026-05-17 02:02:47 +0000

    Clean up old root-level Next.js files causing build conflicts

--- src/components/VisitorTracker.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useEffect } from "react";
-import { useVisitorTracking } from "@/hooks/useVisitorTracking";
-
-export default function VisitorTracker() {
-  useVisitorTracking();
-  return null;
-}
