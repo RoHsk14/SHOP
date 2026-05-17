61d45ae1762eb50376151b210573642a0064e2ca
RoHsk14 <rodriguehessou14@gmail.com>
2026-05-17 02:38:21 +0000

    Delete mobile-app directory

--- mobile-app/lib/config.ts
+++
@@ -1,4 +0,0 @@
-// URL de l'application web Next.js (backend)
-// En développement : utiliser l'IP locale du Mac (pas localhost, car le simulateur iOS y accède depuis lui-même)
-// En production : URL de votre déploiement Vercel
-export const WEB_APP_URL = 'http://192.168.40.235:3000';
